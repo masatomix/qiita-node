cb934c13b8dc626077c8237b3d201e6d44350841
Masatomi KINO <masatomix@ki-no.org>
2020-09-13 00:38:46 +0900

    QiitaからElasticsearchへPOSTするサンプル

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,73 @@
 import request, { Response } from 'request'
 import moment from 'moment-timezone'
 
-const access_token = 'xxx'
+export interface Item {
+  rendered_body: string
+  coediting: boolean
+  comments_count: number
+  created_at: Date
+  group: null
+  id: string
+  likes_count: number
+  private: boolean
+  reactions_count: number
+  tags: Tag[]
+  title: string
+  updated_at: Date
+  url: string
+  user: User
+  page_views_count: number
+}
 
-function getListPromise(pageNumber: number): Promise<Array<any>> {
+export interface Tag {
+  name: string
+  versions: any[]
+}
+
+export interface User {
+  description: string
+  facebook_id: string
+  followees_count: number
+  followers_count: number
+  github_login_name: string
+  id: string
+  items_count: number
+  linkedin_id: string
+  location: string
+  name: string
+  organization: string
+  permanent_id: number
+  profile_image_url: string
+  team_only: boolean
+  twitter_screen_name: string
+  website_url: string
+}
+
+export interface QiitaData {
+  baseDate: Date
+  id: string
+  created_at: Date
+  updated_at: Date
+  title: string
+  page_views_count: number
+  likes_count: number
+  tags: string[]
+  url: string
+}
+
+function getListPromise(access_token: string, pageNumber: number, per_page: number): Promise<Array<Item>> {
   const auth_options = {
-    uri: `https://qiita.com/api/v2/authenticated_user/items?page=${pageNumber}&per_page=100`,
+    uri: 'https://qiita.com/api/v2/authenticated_user/items',
+    qs: {
+      page: pageNumber,
+      per_page: per_page,
+    },
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       Authorization: `Bearer ${access_token}`,
     },
   }
-  const promise: Promise<any> = new Promise((resolve, reject) => {
+  const promise: Promise<Array<Item>> = new Promise((resolve, reject) => {
     request.get(auth_options, (err: any, _response: Response, body: any): void => {
       if (err) {
         reject(err)
@@ -23,7 +79,7 @@ function getListPromise(pageNumber: number): Promise<Array<any>> {
   return promise
 }
 
-function getViews(id: string): Promise<any> {
+function getViewPromise(access_token: string, id: string): Promise<Item> {
   const auth_options = {
     uri: `https://qiita.com/api/v2/items/${id}`,
     headers: {
@@ -31,7 +87,7 @@ function getViews(id: string): Promise<any> {
       Authorization: `Bearer ${access_token}`,
     },
   }
-  const promise: Promise<any> = new Promise((resolve, reject) => {
+  const promise: Promise<Item> = new Promise((resolve, reject) => {
     request.get(auth_options, (err: any, _response: Response, body: any): void => {
       if (err) {
         reject(err)
@@ -45,41 +101,47 @@ function getViews(id: string): Promise<any> {
 
 // https://qiita.com/notakaos/items/3bbd2293e2ff286d9f49
 
-// $ npx ts-node src/index.ts         -> コンソール出力のみ
-// $ npx ts-node src/index.ts true    -> tick_qiita に追加POST
-// $ npx ts-node src/index.ts true qiita  -> qiita にPOST(記事ID_YYYYMMDD をIDとして存在したらUpdate)
+// $ npx ts-node src/index.ts xxx         -> コンソール出力のみ
+// $ npx ts-node src/index.ts xxx true    -> tick_qiita に追加POST
+// $ npx ts-node src/index.ts xxx true qiita  -> qiita にPOST(記事ID_YYYYMMDD をIDとして存在したらUpdate)
 
 if (!module.parent) {
-  const dest = process.argv[3] ? process.argv[3] : 'tick_qiita'
-  const postFlag = process.argv[2] ? process.argv[2].toLowerCase() === 'true' : false
+  const access_token = process.argv[2]
+  const postFlagArg = process.argv[3]
+  const destArg = process.argv[4]
+  const per_page = 100
+
+  const dest = destArg ? destArg : 'tick_qiita'
+  const postFlag = postFlagArg ? postFlagArg.toLowerCase() === 'true' : false
   // console.log(postFlag)
   // console.log(dest)
 
   async function main() {
-    for (let number = 1; number < 30; number++) {
-      const itemList: Array<any> = await getListPromise(number)
-      const filtered = itemList.filter((item) => {
-        // // UiPath タグが付いてる場合OK
-        // const fTags = (item.tags as Array<any>).filter(
-        //   (tag) => tag.name === "UiPath"
-        // );
-        // return fTags.length > 0;
-        return true
-      })
+    for (let number = 1; number < 5; number++) { // デフォルト100x5記事までは検索する
+      const itemList: Array<Item> = await getListPromise(access_token, number, per_page)
+      // const filtered = itemList.filter((item) => {
+      //   // // UiPath タグが付いてる場合OK
+      //   // const fTags = (item.tags as Array<any>).filter(
+      //   //   (tag) => tag.name === "UiPath"
+      //   // );
+      //   // return fTags.length > 0;
+      //   return true
+      // })
 
-      for (const item of filtered) {
-        const result = await getViews(item.id)
+      for (const item of itemList) {
+        // itemList のItemには、page_views_countが入っていないので個別に取得する必要がある。
+        const result: Item = await getViewPromise(access_token, item.id)
         const now = moment()
-        const postData = {
+        const postData: QiitaData = {
           baseDate: now.toDate(),
-          id: result.id,
-          created_at: result.created_at,
-          updated_at: result.updated_at,
-          title: result.title,
+          id: item.id,
+          created_at: item.created_at,
+          updated_at: item.updated_at,
+          title: item.title,
           page_views_count: result.page_views_count,
-          likes_count: result.likes_count,
+          likes_count: item.likes_count,
           tags: item.tags.map((tag: any) => tag.name),
-          url: result.url,
+          url: item.url,
         }
         if (postFlag) {
           // destが tick_qiita のときだけは、都度追加。それ以外は、記事ID_YYYYMMDDと日毎のデータを蓄積
@@ -95,7 +157,7 @@ if (!module.parent) {
         // console.log(row);
       }
 
-      if (itemList.length !== 100) {
+      if (itemList.length !== per_page) {
         break
       }
     }
@@ -104,13 +166,13 @@ if (!module.parent) {
 }
 
 export const postLog = (elastic_url: string, postData: any) => {
-  const optionEla = {
+  const option = {
     url: elastic_url,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     json: postData,
   }
-  request(optionEla, function (error, response, body) {
+  request(option, function (error, response, body) {
     if (!error) {
       console.log(body)
     }
